1596a045c5f83826317e1de0fb3cce99138e65d5
Kamiko Loss <info@gloxi.net>
2024-06-26 00:00:27 +0900

    debug シーンで viewport 設定を動的変更するようにした

--- debug.gd
+++ debug.gd
@@ -1,15 +1,7 @@
 extends Node2D
 
 
-@export var _client_scene: PackedScene 
-
-
-var _client_position_x_list = [0, 360, 720, 1080]
-
-
 func _ready():
-	get_viewport().size = Vector2i(1440, 640)
-	for x in _client_position_x_list:
-		var client = _client_scene.instantiate()
-		client.position.x = x
-		add_child(client)
+	var debug_window_size = Vector2i(1440, 640)
+	get_viewport().size = debug_window_size
+	get_tree().root.content_scale_size = debug_window_size
