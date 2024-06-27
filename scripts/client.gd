23e1c3cf430688ef0c3c57c84b0de870c84c8f10
Kamiko Loss <info@gloxi.net>
2024-06-28 02:47:50 +0900

    Hero の移動タメ実装 (途中)

--- scripts/client.gd
+++ scripts/client.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 
+@export var _hero: Hero
+
 # WebSocket
 @export var _ws_client: WebSocketClient
 @export var _ws_address = "ws://localhost:8000"
@@ -15,15 +17,26 @@ var _ping_refresh_timer: float = 0.0
 var _recent_ping_list: Array[float] = []
 var _recent_ping_list_max_size: int = 50
 
+# UI
+@export var _button_center: Button
+
+# Debug
+@export var _debug_label_state: Label
+@export var _debug_label_charge: Label
+
 
 func _ready() -> void:
 	_ws_client.connected_to_server.connect(_on_web_socket_client_connected_to_server)
 	_ws_client.connection_closed.connect(_on_web_socket_client_connection_closed)
 	_ws_client.message_received.connect(_on_web_socket_client_message_received)
 
+	_button_center.button_down.connect(_on_center_button_down)
+	_button_center.button_up.connect(_on_center_button_up)
+
 
 func _process(delta: float) -> void:
 	_process_refresh_ping(delta)
+	_process_refresh_debug(delta)
 
 
 func _on_web_socket_client_connected_to_server():
@@ -36,6 +49,14 @@ func _on_web_socket_client_connection_closed():
 
 func _on_web_socket_client_message_received(message: Variant):
 	print("[Client] Message received from server. Message: %s" % [message])
+
+
+func _on_center_button_down():
+	_hero.enter_charge()
+
+
+func _on_center_button_up():
+	_hero.exit_charge()
 
 
 func _connect_to_server():
@@ -57,3 +78,9 @@ func _process_refresh_ping(delta: float) -> void:
 	var ping_avg = ping_sum / _recent_ping_list.size()
 	_ping_current_label.text = "Current: %s ms" % str(snappedf(_recent_ping_list[-1] * 1000, 0.01))
 	_ping_average_label.text = "Average: %s ms" % str(snappedf(ping_avg * 1000, 0.01))
+
+
+# Debug
+func _process_refresh_debug(_delta: float) -> void:
+	_debug_label_state.text = "STAT:%s" % Hero.MoveState.keys()[_hero.move_state]
+	_debug_label_charge.text = "CHRG:%s" % snapped(_hero.charge, 0.01)
--- scripts/hero.gd
+++ scripts/hero.gd
@@ -2,26 +2,47 @@ extends RigidBody2D
 class_name Hero
 
 
-@export var _sprite: Sprite2D
-@export var _arrow_label: Label
-
-var _current_direction: float = 90.0 # 現在の移動方向 (deg)
-var _last_direction: float = 90.0 # 最後に移動した方向 (deg)
-@export var _default_arrow_rotation_speed: int = 90 # 移動方向の矢印の回転速度 (deg/s)
-
-var _move_tween: Tween
-
-
 signal move_started # 移動が開始した
 signal move_finished # 移動が終了した
 
 
 # 移動状態
 enum MoveState {
-	WAITING, # 入力待ち
-	MOVEING, # 移動中
-	COOLING, # 移動後のクールタイム
+	WAITING, # 何もしていない/移動タメ開始待ち
+	CHARGING, # 移動タメ中/移動タメ終了待ち
+	MOVEING, # 移動中/移動終了待ち
+	COOLING, # クールタイム中/クールタイム終了待ち
 }
+
+
+var move_state = MoveState.WAITING
+
+@export var _sprite: Sprite2D
+@export var _arrow: Control
+@export var _arrow_square: TextureRect
+
+var _direction: float = 90.0 # 現在の移動方向 (deg)
+@export var _direction_rotation_speed_default: float = 90.0
+var _direction_rotation_speed: float = _direction_rotation_speed_default # 移動方向の矢印の回転速度 (deg/s)
+
+var charge: float = 0.0 # 現在の移動タメ度 (最大 1.0)
+@export var _charge_speed_default: float = 0.2
+var _charge_speed: float = _charge_speed_default  # 移動タメの速度 (/s)
+@export var _charge_duration_default: float = 1.0
+var _charge_duration = _charge_duration_default # 移動タメが最大に達するまでの秒数 (s)
+
+var _move_tween: Tween:
+	get:
+		if _move_tween:
+			_move_tween.kill()
+		_move_tween = create_tween()
+		return _move_tween
+var _arrow_square_tween: Tween:
+	get:
+		if _arrow_square_tween:
+			_arrow_square_tween.kill()
+		_arrow_square_tween = create_tween()
+		return _arrow_square_tween
 
 
 func _ready() -> void:
@@ -32,15 +53,35 @@ func _process(delta: float) -> void:
 	_process_rotate_direction(delta)
 
 
-# direction の向きに strength の強さで移動する
-func move(strength: float) -> void:
-	pass
+# 移動のタメを開始する
+func enter_charge() -> void:
+	move_state = MoveState.CHARGING
+
+	_move_tween.set_loops()
+	_move_tween.set_parallel(true)
+	_move_tween.tween_method(func(v): charge = _charge_speed * v, 0.0, 1.0, _charge_duration)
+	_move_tween.tween_property(_arrow_square, "scale.x", 1.0, _charge_duration)
+
+
+# 移動のタメを終了する
+func exit_charge() -> void:
+	move_state = MoveState.COOLING
+
+	_move_tween.set_parallel(true)
+	_move_tween.set_ease(Tween.EASE_OUT)
+	_move_tween.set_trans(Tween.TRANS_QUINT)
+	charge = 0.0
+	_move_tween.tween_property(_arrow_square, "scale.x", 0.0, _charge_duration / 4)
+
+	move_started.emit()
 
 
 func _process_rotate_direction(delta: float) -> void:
-	var speed = _default_arrow_rotation_speed
-	_current_direction += speed * delta
-	if 360.0 < _current_direction:
-		_current_direction -= 360.0
+	if move_state != MoveState.WAITING:
+		return
 
-	_arrow_label.rotation_degrees = _current_direction
+	_direction += _direction_rotation_speed * delta
+	if 360.0 < _direction:
+		_direction -= 360.0
+
+	_arrow.rotation_degrees = _direction
